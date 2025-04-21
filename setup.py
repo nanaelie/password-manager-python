ac2f62c570bde45e541532315d0371df87a43567
Nana Elie <91574567+nanaelie@users.noreply.github.com>
2025-04-21 23:37:29 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,30 +1,27 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="pmgr",
-    version="1.0.0",
-    author="nanaelie",
-    description="A Python-based password manager with features for storing, generating, and managing passwords securely.",
-    long_description=open("README.md", encoding="utf-8").read(),
-    long_description_content_type="text/markdown",
-    url="https://github.com/nanaelie/password-manager-python",
+    name='psmgr',
+    version='1.0.0',
+    author='nanaelie-dev',
+    description='A simple and secure password manager built in Python.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/nanaelie/psmgr',
     packages=find_packages(),
-    python_requires=">=3.6",
     install_requires=[
-        "pandas"
+        'pandas',
     ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
     entry_points={
         'console_scripts': [
             'pmgr=main:main',
-        ],
+        ]
     },
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Topic :: Security",
-        "Intended Audience :: End Users/Desktop",
-        "Environment :: Console",
-    ],
     include_package_data=True,
 )
